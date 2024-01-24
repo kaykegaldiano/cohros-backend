ec3258e6fbf78b807595556bcda7006520c13845
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-01-24 16:33:10 -0300

    creating Contact and Phone entities

---
+++ tests/Feature/CreateContactTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Model\User;
+
+it('can create a contact', function () {
+    // $user = Mockery::mock(User::class);
+    // $user->shouldReceive('setName')->with('John Doe');
+    // $user->shouldReceive('setEmail')->with('john@doe.com');
+    // $user->shouldReceive('setPassword')->with('password');
+    expect(true)->toBe(true);
+});
