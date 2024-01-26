1180700f9ab0904a0b5454560ce729b0c3b6e9bb
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-01-26 17:26:46 -0300

    update routes

--- config/routes.php
+++ config/routes.php
@@ -2,8 +2,10 @@
 
 use App\Controller\ListContacts;
 use App\Controller\PersistContact;
+use App\Controller\PersistUser;
 
 return [
+    '/create-user' => PersistUser::class,
     '/list-contacts' => ListContacts::class,
     '/save-contact' => PersistContact::class,
 ];
