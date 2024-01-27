b4ec18cd9e1b6bfb98a7914e1a28ead591b73fdc
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-01-27 05:22:05 -0300

    wip

--- app/Controller/PersistContact.php
+++ app/Controller/PersistContact.php
@@ -6,6 +6,8 @@ use App\Model\Contact;
 use App\Model\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -23,12 +25,34 @@ class PersistContact implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $contactName = htmlspecialchars(filter_var($request->getParsedBody()['name'], FILTER_SANITIZE_SPECIAL_CHARS));
+        $authorizationHeader = $request->getHeaders()['Authorization'][0];
+        $token = str_replace('Bearer ', '', $authorizationHeader);
+
+        try {
+            $decodedToken = JWT::decode($token, new Key($_ENV['JWT_SECRET'], 'HS256'));
+
+//            return new Response(201, ['Content-Type' => 'application/json'], json_encode($decodedToken));
+        } catch (\Throwable $t) {
+            return new Response(401, ['Content-Type' => 'application/json'], json_encode([
+                'status' => 'error',
+                'message' => $t->getMessage(),
+            ]));
+        }
+
+        $data = json_decode($request->getBody()->getContents());
+
+        $contactName = filter_var($data->name, FILTER_SANITIZE_SPECIAL_CHARS);
+        $contactEmail = filter_var($data->email, FILTER_VALIDATE_EMAIL);
+        $contactAddress = filter_var($data->address, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        //        $contactName = htmlspecialchars(filter_var($request->getParsedBody()['name'], FILTER_SANITIZE_SPECIAL_CHARS));
         $contactId = filter_var($request->getQueryParams()['id'], FILTER_VALIDATE_INT);
 
         if (false !== $contactId) {
             $contact = $this->entityManager->find(Contact::class, $contactId);
             $contact->setName($contactName);
+            $contact->setEmail($contactEmail);
+            $contact->setAddress($contactAddress);
             $this->entityManager->flush();
 
             return new Response(200, ['Content-Type' => 'application/json'], json_encode([
@@ -37,9 +61,11 @@ class PersistContact implements RequestHandlerInterface
             ]));
         }
 
-        $user = $this->userRepository->findOneBy(['email' => 'test@test.com']);
+        $user = $this->userRepository->findOneBy(['email' => $decodedToken->email]);
         $contact = new Contact();
         $contact->setName($contactName);
+        $contact->setEmail($contactEmail);
+        $contact->setAddress($contactAddress);
         $contact->setUser($user);
         $user->getContacts()->add($contact);
         $this->entityManager->persist($contact);
--- app/Model/Contact.php
+++ app/Model/Contact.php
@@ -25,10 +25,10 @@ class Contact
     private string $address = '';
 
     #[ORM\Column(type: 'datetime')]
-    private \DateTimeImmutable $createdAt;
+    private \DateTime $createdAt;
 
-    #[ORM\Column(type: 'datetime')]
-    private \DateTimeImmutable $updatedAt;
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private \DateTime $updatedAt;
 
     /** @var Collection<string, Phone> */
     #[ORM\OneToMany(mappedBy: 'contact', targetEntity: Phone::class)]
@@ -40,7 +40,7 @@ class Contact
 
     public function __construct()
     {
-        $this->createdAt = new \DateTimeImmutable('now');
+        $this->createdAt = new \DateTime('now');
         $this->phones = new ArrayCollection();
     }
 
@@ -79,17 +79,17 @@ class Contact
         $this->address = $address;
     }
 
-    public function getCreatedAt(): \DateTimeImmutable
+    public function getCreatedAt(): \DateTime
     {
         return $this->createdAt;
     }
 
-    public function getUpdatedAt(): \DateTimeImmutable
+    public function getUpdatedAt(): \DateTime
     {
         return $this->updatedAt;
     }
 
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): void
+    public function setUpdatedAt(\DateTime $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
     }
